cc1a798f24bccf5744c76c6060b9efb2c8a287f5
Fatehaider <fatehhaider111@gmail.com>
2025-10-18 00:28:13 +0500

    Add comparison operators example to class_07.py

---
+++ class_07.py
@@ -0,0 +1,24 @@
+# Comparison Operators
+# Base values
+n1=1131
+n2 =1050
+# equalto 
+equalto = n1 == n2
+# print(f"{n1} == {n2} : {equalto}")
+#not equal to
+notequalto = n1 != n2
+# print(f"{n1} != {n2} : {notequalto}")
+# greater than (>  )    
+greaterthan = n1 > n2
+# print(f"{n1} > {n2} : {greaterthan}")
+# less than (< )
+lessthan = n1 < n2
+# print(f"{n1} < {n2} : {n1< n2}")
+# greater than equal to (>=)
+greaterthanequalto = n1 >= n2   
+# print(f"{n1} >= {n2} : {greaterthanequalto}")
+# less than equal to (<=)
+lessthanequalto = n1 <= n2  
+print(f"{n1} <= {n2} : {lessthanequalto}")
+
+
